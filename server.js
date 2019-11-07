db71863ebedc2dc2edf42e7903a28083af074125
Glitch (hello-express) <none>
2019-11-07 00:11:39 +0000

    🎩🐏 Checkpoint ./server.js:6410015/3779 ./views/index.ejs:6410015/745

--- server.js
+++ server.js
@@ -2,44 +2,52 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
 
-var bodyPraser = require('body-parser');
-var urlencodedPraser = bodyPraser.urlencoded({extended: false});
+var bodyPraser = require("body-parser");
+var urlencodedPraser = bodyPraser.urlencoded({ extended: false });
 
-app.use(bodyPraser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyPraser.urlencoded({
+    extended: true
+  })
+);
 
 //load my .json file of english dictionary word
-const manager = require('./manager.json');
-const product = require('./product.json');
+const manager = require("./manager.json");
+const product = require("./product.json");
 
 //use the static files in the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //tell express where to get your views and which template engine to use
 app.set("views", __dirname + "/views/");
 app.set("view engine", "ejs");
 
-
-
 //define your routes here. don't forget about error handling
-app.get('/', function(request, response) {
+app.get("/", function(request, response) {
   response.render("index", {
-    message: "Hey everyone! This is my webpage.",
+    message: "Hey everyone! This is my webpage."
   });
 });
 
-
-
-
-
-
-
+app.post("/register", function(req, res, next) {
+  manager.findOne({ username: req.body.username }, function(err, doc) {
+    if (err) throw err;
+    if (doc) {
+      // the name already exists
+      res.json({ Failed: true, message: "the name already exists" });
+    } else {
+      manager.create(req.body, function(err, post) {
+        if (err) return next(err);
+        res.json({ Failed: false, message: "User Created !" });
+      });
+    }
+  });
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
