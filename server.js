a6fdaf178767c10d87019aee5b203196a48bf9c2
Glitch (hello-express) <none>
2019-11-07 00:41:25 +0000

    🐫🐯 Checkpoint ./server.js:6410015/13353 ./views/register.ejs:6410015/3938 ./views/index.ejs:6410015/1

--- server.js
+++ server.js
@@ -2,57 +2,83 @@
 // where your node app starts
 
 // init project
-const express = require("express");
+const express = require('express');
 const app = express();
-var router = express.Router();
 
-var bodyPraser = require("body-parser");
-var urlencodedPraser = bodyPraser.urlencoded({ extended: false });
+var bodyPraser = require('body-parser');
+var urlencodedPraser = bodyPraser.urlencoded({extended: false});
 
-app.use(
-  bodyPraser.urlencoded({
-    extended: true
-  })
-);
+app.use(bodyPraser.urlencoded({
+  extended: true
+}));
 
-//load my .json file of english dictionary word
-const manager = require("./manager.json");
-const product = require("./product.json");
+//load my .json file
+const manager = require('./manager.json');
+const product = require('./product.json');
 
 //use the static files in the public folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //tell express where to get your views and which template engine to use
 app.set("views", __dirname + "/views/");
 app.set("view engine", "ejs");
 
+
+
 //define your routes here. don't forget about error handling
-app.get("/", function(request, response) {
+app.get('/', function(request, response) {
   response.render("index", {
-    message: "Hey everyone! This is my webpage."
+    message: "Hey everyone! This is my webpage.",
   });
 });
 
-// app.post("/register", function(req, res, next) {
-//   manager.findOne({ username: req.body.username }, function(err, doc) {
-//     if (err) throw err;
-//     if (doc) {
-//       // the name already exists
-//       res.json({ Failed: true, message: "the name already exists" });
-//     } else {
-//       manager.create(req.body, function(err, post) {
-//         if (err) return next(err);
-//         res.json({ Failed: false, message: "User Created !" });
-//       });
-//     }
-//   });
+app.get('/products', function(request, response, next) {
+  try
+  {
+    response.render("products", {product_key: product});
+  }
+  catch(err){
+    next(err);
+  }
+});
+
+// app.get('/list', function(request, response, next) {
+//   try
+//   {
+//     response.render("list", {
+//       item1:movie_data,
+//       item2:movie_info_data,
+//     });
+//   }
+//   catch(err){
+//     next(err);
+//   }
 // });
 
-router.get('/register2', function(req, res, next) {
-  res.render('views/register', {title: 'index'});
+
+
+
+//Capture 404 and direct to error handling
+app.use(function(request, response, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+//Error handling in production environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+    layout: false
+  });
+});
+
+
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
