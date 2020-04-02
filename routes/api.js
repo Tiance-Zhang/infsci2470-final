76531152bb80917734abf58d875a560f9a3fd439
Glitch (hello-express) <none>
2020-04-02 15:58:17 +0000

    🍂👶 Checkpoint ./views/addNew.ejs:10221703/1 ./views/added.ejs:10221703/79 ./views/index.ejs:10221703/1 ./views/product.ejs:10221703/79 ./views/vendors.ejs:10221703/90 ./routes/api.js:10221703/5132 ./server.js:10221703/4930

--- routes/api.js
+++ routes/api.js
@@ -5,162 +5,52 @@ const router = express.Router()
 const Product = require("../models/product");
 const Vendor = require("../models/vendor");
 
-// RETREIVE all resources
-router.get("/list", function(req,res){
-  Product.find({}, function (err, product_list){
-    Vendor.find({}, function (err, vendor_list){
-      if(err){
-      res.status(500).send(err);
-    }else{
-      res.json({product:product_list,vendor:vendor_list});
-    }
-    });
-    
-  });
-  
-});
 
-router.get("/product", function(req,res){
+//import data models
+
+
+// RETREIVE all books
+router.get("/", function(req,res){
   Product.find({}, function (err, product_list){
-    if(err){
-      res.status(500).send(err);
-    }else{
     res.json(product_list);
-    }
   });
 });
 
-// router.get("/vendor", function(request, response, next) {
-//   Vendor.find({}, function(err, vendor_list) {
-//     response.json(vendor_list);
-//   });
-// });
-router.get("/vendor", function(req,res){
-  Vendor.find({}, function (err, vendor_list){
-    if(err){
-      res.status(500).send(err);
-    }else{
-    res.json(vendor_list);
-    }
-  });
-});
-// retrive a specific product
+// RETRIEVE a specific book
 router.get("/:productId", function(req, res){
   Product.findById(req.params.productId, function(err, product) {
-    if(err){
-      res.status(500).send(err);
-    }else{
     res.json(product)
-    }
   });
 });
 
-// retrive a specific vendor
-router.get("/:vendorId/vendor", function(req, res){
-  Vendor.findById(req.params.vendorId, function(err, vendor) {
-    if(err){
-      res.status(500).send(err);
-    }else{
-    res.json(vendor)
-    }
-  });
-});
-
-//CREATE new product
-router.post('/newproduct', function(req, res){
+//CREATE
+router.post('/', function(req, res){
   let product = new Product(req.body);
   product.save();
   res.status(201).send(product);
 });
 
-//CREATE multiple new products
-router.post("/newproducts", function(req, res) {
-  Product.estimatedDocumentCount({}, function(err, count) {
-    
-    let product = req.body;
-    for (let i = 0; i < product.length; i++) {
-      let product = new Product(product[i]);
-      
-      product.save();
-      
-    }
-    res.status(201).send(product);
-  });
-});
-
-
-//CREATE new vendor
-router.post('/newvendor', function(req, res){
-  let vendor = new Vendor(req.body);
-  vendor.save();
-  res.status(201).send(vendor);
-});
-
-//UPDATE  product
-/*router.put("/product/:productId", function(req, res) {
+//UPDATE
+router.put("/:productId", function(req, res) {
   Product.findById(req.params.productId, function(err, product) {
-    if(err){
-      res.status(500).send(err);
-    }else{
-    product.id = req.body.id;
-    product.name = req.body.name;
-    product.size = req.body.size;
-    product.price = req.body.price;
-    product.volumn = req.body.volumn;
-      
+    product.title = req.body.title;
+    product.author = req.body.author;
     product.save();
     res.json(product);
-    }
   });
 });
 
-//UPDATE the vendor
-router.put("/vendor/:vendorId", function(req, res) {
-  Vendor.findById(req.params.vendorId, function(err, vendor) {
-    if(err){
-      res.status(500).send(err);
-    }else{
-    vendor.id = req.body.id;
-    vendor.name = req.body.name;
-    vendor.email = req.body.email
-    vendor.tel = req.body.tel;
-    vendor.product = req.body.product;
-
-    vendor.save();
-    res.json(vendor);
-    }
-  });
-});
-
-//DELETE the product
-router.delete("/product/:productId", function(req, res){
+//DELETE
+router.delete("/:productId", function(req, res){
   Product.findById(req.params.productId, function(err, product) {
     product.remove(function(err){
         if(err){
           res.status(500).send(err);
         }
         else{
-          res.status(204).send('Its removed');
+          res.status(204).send('removed');
         }
     });
   });
 });
-
-//DELETE the vendor
-router.delete("/vendor/:vendorId", function(req, res){
-  Vendor.findById(req.params.vendorId, function(err, vendor) {
-    vendor.remove(function(err){
-        if(err){
-          res.status(500).send(err);
-        }
-        else{
-          res.status(204).send('Its removed');
-        }
-    });
-  });
-});
-
-*/
-
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
- console.log("In server.js!");
 
 // init project
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 
 // Establish a connection with the Mongo Database
 // Get the username, password, host, and databse from the .env file
@@ -15,24 +15,8 @@ const mongoDB = ("mongodb+srv://"+
                  +process.env.HOST+
                  "/"
                  +process.env.DATABASE);
-// console.log("Connection String: "+mongoDB);
-
 mongoose.connect(mongoDB, {useNewUrlParser: true, retryWrites: true});
 
-//debugging 
-mongoose.connection.on('connected', function (){
-  console.log('Mongoose connected to '+process.env.DATABASE);
-});
-
-mongoose.connection.on('error', function (err){
-  console.log('Mongoose connection error: '+err);
-});
-
-mongoose.connection.on('disconnected', function (){
-  console.log('Mongoose disconnected.');
-});
-
-//start express 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,12 +26,11 @@ app.set("view engine", "ejs")
 app.set("views", __dirname + "/views/");
 
 // Load routes
-// const apiRouter = require("./routes/api");
+const apiRouter = require("./routes/api");
 const indexRouter = require("./routes/index");
 
 app.use("/", indexRouter);
-// app.use("/api/product", apiRouter);
-// app.use(express.static("public"));
+app.use("/api/product", apiRouter);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
