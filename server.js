f0d9430fb280b34d7f27594d933814e800194b24
Glitch (sumptuous-joyous-soul) <none>
2020-04-18 00:54:08 +0000

    💴🍍 Checkpoint ./server.js:10221703/3454 ./package.json:10221703/5352

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
+
 
 
 // Establish a connection with the Mongo Database
@@ -52,11 +51,6 @@ const indexRouter = require("./routes/index");
 
 
 
-
-
-
-
-
 //app.use("/view/product",indexRouter);
 app.use("/", indexRouter);
 app.use("/api/", apiRouter);
@@ -67,3 +61,60 @@ app.use("/api/", apiRouter);
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//copy
+
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
