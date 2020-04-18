abec3dc3c455a004408a7802196d3907575bbd10
Glitch (sumptuous-joyous-soul) <none>
2020-04-18 01:42:03 +0000

    🍯🍙 Checkpoint ./views/index.ejs:10214623/663 ./config/auth.js:10221703/136 ./server.js:10221703/43

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.use(function(req, res, next) {
 });
 
 // Routes
-app.use('/', require('./routes/index.js'));
+
 app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 3000;
