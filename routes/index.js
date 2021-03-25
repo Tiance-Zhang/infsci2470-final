64105b4db1614a3fe7aa21d34e78139302ce5de4
Glitch (sumptuous-joyous-soul) <none>
2021-03-25 02:50:11 +0000

    👰🍠 Checkpoint ./routes/index.js:10221703/602:10370709/44 ./views/cart.ejs:10370709/10

--- routes/index.js
+++ routes/index.js
@@ -148,17 +148,18 @@ router.post("/clearCart", function(req, res) {
   my_cart.save();
   res.redirect("/myCart");
 });
-
 //update
 router.put("/update", function(req, res) {
   Cart.find({ email: req.user.email }, function(err, cart_item) {
-    if (!cart_item.length) {
+    if (!cart_item.length) {//////
       my_cart = new Cart();
       my_cart.email = req.user.email;
       my_cart.save();
     } else {
       my_cart = cart_item[0];
     }
+  });
+  
   let pid = req.query.id;
   let index = 0;
   for (index = 0; index < my_cart.product_list.length; index++) {
@@ -168,9 +169,7 @@ router.put("/update", function(req, res) {
       break;
     }
   }
-  res.redirect("/myCart"); 
-  });
-
+  res.redirect("/myCart");
 });
 
 router.get("/addNew", function(request, response, next) {
