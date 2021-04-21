b16d9f81c2cc045f39b011d8724d4f5c3bb6c367
Glitch (sumptuous-joyous-soul) <none>
2021-04-21 20:41:32 +0000

    🎈🌈 Checkpoint ./models/product.js:10221703/72

--- models/product.js
+++ models/product.js
@@ -6,9 +6,11 @@ const productSchema = new Schema(
     Task_id: {type: Number},
     TaskName: {type: String},
     Instructor: {type: String},
+    isred: {type: String},
     status: {type: Number},
     Room: {type: String},
     Description: {type: String},
+    
   }
 );
 
